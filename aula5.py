5de70ef97fac004385016ce66e08263312d66f53
gj2071 <143042340+gj2071@users.noreply.github.com>
2024-08-27 21:37:09 -0300

    Add files via upload

---
+++ aula5.py
@@ -0,0 +1,54 @@
+print('SISTEMA DE NOTAS')
+
+lista_nomes = []
+lista_notas = []
+notas_aluno1 = []
+notas_aluno2 = []
+notas_aluno3 = []
+
+
+nome1 = input('Nome:')
+nota_aluno1_a =  float(input('Nota:'))
+nota_aluno2_b =  float(input('Nota:'))
+nota_aluno3_c =  float(input('Nota:'))
+notas_aluno1 +=(nota_aluno1_a, nota_aluno2_b, nota_aluno3_c)
+media_aluno1  = sum(notas_aluno1) / len(notas_aluno1)
+print('Media aluno', nome1, media_aluno1)
+
+
+
+
+
+# nome2 = input('Nome:')
+# nota2 =  float(input('Nota:'))
+# nota2 =  float(input('Nota:'))
+# nota2 =  float(input('Nota:'))
+
+
+# nome3 = input('Nome:')
+# nota3 =  float(input('Nota:'))
+# nota3 =  float(input('Nota:'))
+# nota3 =  float(input('Nota:'))
+
+
+# lista_nomes.append(nome1)
+# lista_nomes.append(nome2)
+# lista_nomes.append(nome3)
+
+# lista_notas.append(nota1)
+# lista_notas.append(nota2)
+# lista_notas.append(nota3)
+
+
+# media =  sum(lista_notas)/ len(lista_notas)
+# print(lista_nomes)
+# print(lista_notas)
+
+# print('Media', media)
+
+
+
+
+
+
+
